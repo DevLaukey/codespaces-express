a5fb735ef5c97f270cf128eb37e92b9e85d49026
DevLaukey <63729244+DevLaukey@users.noreply.github.com>
2022-11-17 18:24:01 +0000

    phone number remaining

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
 const express = require("express");
-const smsRouter = require("./routes/smsRouter").default;
+const smsRouter = require("./routes/smsRouter");
 
 const app = express();
-const port = process.env.PORT || 8013;
+const port = process.env.PORT || 8011;
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
-app.post("/", async (req, res) => {
-  try {
-    let { event_date, event_name, phone_number } = req.body;
-
-    const accountSid = "AC37f9f4143bdea7ea687b7738c6768a0f";
-    const authToken = "5b3c3b62f258c0c680fa119301d8ed66";
-    const client = require("twilio")(accountSid, authToken);
-
-    client.messages
-      .create({
-        body: `Hello This is a reminder of ${event_name} from OnTask on date ${event_date} `,
-        from: "+13608032172",
-        to: phone_number,
-      })
-      .then((message) => res.send(message.sid)).catch((err) => console.log(err.message))
-  } catch (error) {
-    console.log("error", error);
-  }
-});
+app.post("/",smsRouter);
 
 app.get("/li", (req, res )=> res.send("ththth"))
 app.listen(port, () => {
